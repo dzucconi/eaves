f3679671e254afafee6ca3d1040e7bef041f9f57
dzucconi <mail@damonzucconi.com>
2017-01-26 17:46:53 +0100

    Fix migration ordering

--- db/migrate/20170126160827_create_users_and_emails.rb
+++ db/migrate/20170126160827_create_users_and_emails.rb
@@ -1,5 +1,11 @@
-class CreateEmails < ActiveRecord::Migration[5.0]
+class CreateUsersAndEmails < ActiveRecord::Migration[5.0]
   def change
+    create_table :users do |t|
+      t.string :email
+
+      t.timestamps
+    end
+
     create_table :emails do |t|
       t.string :from
       t.text :body
