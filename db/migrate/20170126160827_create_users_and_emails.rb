d6980945921a0e84af214596b43e6d7828d44045
dzucconi <mail@damonzucconi.com>
2017-01-27 15:15:30 +0100

    Adds AWS SDK

--- db/migrate/20170126160827_create_users_and_emails.rb
+++ db/migrate/20170126160827_create_users_and_emails.rb
@@ -2,6 +2,7 @@ class CreateUsersAndEmails < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :email
+      t.index :email, unique: true
 
       t.timestamps
     end
@@ -11,6 +12,8 @@ class CreateUsersAndEmails < ActiveRecord::Migration[5.0]
       t.text :body
       t.string :to
       t.string :subject
+      t.string :message_id
+      t.index :message_id, unique: true
       t.references :user, foreign_key: true
 
       t.timestamps
