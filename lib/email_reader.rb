3a8282ef9c23ec531606a21154badb696f47e325
dzucconi <mail@damonzucconi.com>
2017-01-27 15:17:01 +0100

    Implements reader using AWS + Mail

---
+++ lib/email_reader.rb
@@ -0,0 +1,30 @@
+require 'mail'
+
+class EmailReader
+  attr_reader :message_id
+
+  def initialize(message_id)
+    @message_id = message_id
+  end
+
+  def message
+    @message ||= Aws::S3::Resource
+      .new
+      .bucket(ENV['AWS_S3_BUCKET'])
+      .object(message_id)
+      .get
+  end
+
+  def read
+    @contents ||= message.body.read
+  end
+
+  def envelope
+    @envelope ||= Mail.read_from_string(read)
+  end
+
+  def persist
+    user = User.find_or_create_by(email: envelope.from.first)
+    user.emails.find_or_create_by(message_id: message_id)
+  end
+end
