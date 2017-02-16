f1d9033d54f046fffcca0578fb791a438102395c
dzucconi <mail@damonzucconi.com>
2017-02-16 17:34:55 +0100

    Nicer flash handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def render_flash
+    ActiveSupport::SafeBuffer.new.tap do |output|
+      %i(notice success error).each do |message|
+        output << content_tag(:div, class: "alert alert--#{message}") do
+          flash[message]
+        end if flash[message].present?
+        flash[message] = nil
+      end
+    end
+  end
 end
